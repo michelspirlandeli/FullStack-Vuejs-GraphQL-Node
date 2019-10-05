6f013fa2d95b33078f6a6311dcdf58c37dd3be52
michelspirlandeli <michelspirlandeli10@gmail.com>
2019-10-05 17:05:28 -0300

    evoluindo o projeto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,144 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <br />
+      <h6 class="text-secondary">Gerador de nomes utilizando vuejs, GrapQL e node</h6>
+    </div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <h5>
+              Prefixo
+              <span class="badge badge-info">{{prefixes.length}}</span>
+            </h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                    <div class="row">
+                      <div class="col-md">
+                          {{ prefix}}
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" @click="deletePrefix(prefix)"><span class="fa fa-trash"></span></button>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+                <br/>
+                <div class="input-group">
+                    <input type="text" class="form-control" v-model="prefix" v-on:keyup.enter="addPrefix(prefix)" placeholder="digite o prefixo">
+                    <div class="input-group-append">
+                      <button class="btn btn-info" @click="addPrefix(prefix)"><span class="fa fa-plus"></span></button>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <h5>sufixos
+              <span class="badge badge-info">{{sufixes.length}}</span>
+            </h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                    <div class="row">
+                      <div class="col-md">
+                          {{ sufix}}
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" @click="deleteSufix(sufix)"><span class="fa fa-trash"></span></button>
+                      </div>
+                    </div>
+                    </li>
+                </ul>
+                <br/>
+                <div class="input-group">
+                    <input type="text" class="form-control" v-model="sufix" v-on:keyup.enter="addSufix(sufix)" placeholder="digite o sufixo">
+                    <div class="input-group-append">
+                      <button class="btn btn-info" @click="addSufix(sufix)"><span class="fa fa-plus"></span></button>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <h5>Domínios <span class="badge badge-info">{{domains.length}}</span></h5>
+      <div class="card">
+        <div class="card-body">
+          <ul class="list-group">
+            <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
+              {{domain}}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	name: "app",
+	data(){
+		return{
+			prefix:"",
+			sufix:"",
+			prefixes:["Air", "Jet", "Flight"],
+			sufixes:["Hub","Station", "marte"],
+			domains: ["AirHub", "AirStation", "AirMarte", "JetHub", "jetStation", "jetMart", "Flighthub", "FlightStation", "FlightMarte"]
+		};
+	},
+	methods:{
+		addPrefix(prefix){
+			this.prefixes.push(prefix);
+			this.prefix = "";
+			this.generate();
+		},
+		addSufix(sufix){
+			this.sufixes.push(sufix);
+			this.sufix = "";
+			this.generate();
+		},
+		deletePrefix(prefix){
+			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+			this.generate();
+		},
+		deleteSufix(sufix){
+			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+			this.generate();
+		},
+		generate(){
+			this.domains = []; {
+				for(const prefix of this.prefixes){
+					for(const sufix of this.sufixes){
+						this.domains.push(prefix + sufix);
+					}
+				}
+       
+			}
+		}
+	}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
 }
 </style>
+
